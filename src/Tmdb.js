3eb9e7300070348f04c0122603eb0749eeb6dccb
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-06-20 15:45:25 -0300

    netflix-react

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -33,7 +33,7 @@ export default {
         slug: "action",
         title: "Ação",
         items: await basicFetch(
-          `/discover/movie?with_genres=28?language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=35?language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
@@ -47,7 +47,7 @@ export default {
         slug: "horror",
         title: "Terror",
         items: await basicFetch(
-          `/discover/movie?with_genres=27?language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=28?language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
